626598a1b0000825a498f9760617374691ba8519
Jbrak <joe@brakoniecki.com>
2019-10-02 20:56:14 +0100

    Used loops

--- app/routes.py
+++ app/routes.py
@@ -4,4 +4,12 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', title = 'Weather Data', temp = 22)
+    temp = [
+    {'temp' : '20',
+     'time' : '8:30'},
+
+     {'temp' : '24',
+      'time' : '9:00'}
+    ]
+
+    return render_template('index.html',temp = temp)
