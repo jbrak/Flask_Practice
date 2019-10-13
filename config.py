250720a992604525317bd4d1a2b851cc7c1d522e
Jbrak <joe@brakoniecki.com>
2019-10-13 19:48:29 +0100

    Updated

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
+    #Forms
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+
+    #DataBase
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
