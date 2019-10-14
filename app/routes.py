2c4ff3614b381cb85c14edf8cc87f9de5fc12c16
dbrak <david@brakoniecki.com>
2019-10-14 17:43:12 +0100

    Adding code to take data from the database

--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,19 @@
 from flask import render_template
 from app import app
 from app.forms import SubmitData
+from app import db
+from app.models import Data
 
 @app.route('/')
 @app.route('/index')
 def index():
-    temp = [
-    {'temp' : '20',
-     'time' : '8:30'},
-
-     {'temp' : '24',
-      'time' : '9:00'}
-    ]
+    temp = db.session.query(Data).all()
+    # temp = [
+    # {'temp' : '20',
+    # 'time' : '8:30'},
+    #  {'temp' : '24',
+    #   'time' : '9:00'}
+    # ]
 
     return render_template('index.html',title = 'Bransgore' ,temp = temp)
 
