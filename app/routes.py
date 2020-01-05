e9b0843eb83463c0daca468df1b52cc2b9b92ca0
Jbrak <joe@brakoniecki.com>
2020-01-05 16:13:18 +0000

    Writes data to data base every time page refreshes

--- app/routes.py
+++ app/routes.py
@@ -2,10 +2,18 @@ from flask import render_template
 from app import app
 from app import db
 from app.models import Data
+from datetime import datetime
 
 @app.route('/')
 @app.route('/index')
 def index():
+
+    #Sample writing Data
+    db.session.rollback()
+    d = Data(date = datetime.now(), temp = 333.8)
+    db.session.add(d)
+    db.session.commit()
+
     temp = db.session.query(Data).all()
 
     return render_template('index.html',title = 'Bransgore' ,temp = temp)
