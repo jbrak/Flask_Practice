21078513ae2345bb14ebf27996d4b4503c600ad7
Jbrak <joe@brakoniecki.com>
2020-01-05 16:56:59 +0000

    Added Scheduler

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,30 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from apscheduler.schedulers.background import BackgroundScheduler
 
 app = Flask(__name__)
+
+#Database Config
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+#Scheduler
+from app.models import Data
+from datetime import datetime
+
+scheduler = BackgroundScheduler()
+
+def write():
+    #Sample writing Data
+    db.session.rollback()
+    d = Data(date = datetime.now(), temp = 333.8)
+    db.session.add(d)
+    db.session.commit()
+
+
+scheduler.add_job(func=write, trigger="interval", seconds = 5)
+scheduler.start()
 
 from app import routes, models
--- app/routes.py
+++ app/routes.py
@@ -2,17 +2,11 @@ from flask import render_template
 from app import app
 from app import db
 from app.models import Data
-from datetime import datetime
 
 @app.route('/')
 @app.route('/index')
 def index():
 
-    #Sample writing Data
-    db.session.rollback()
-    d = Data(date = datetime.now(), temp = 333.8)
-    db.session.add(d)
-    db.session.commit()
 
     temp = db.session.query(Data).all()
 
